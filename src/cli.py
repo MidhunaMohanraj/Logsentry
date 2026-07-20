8362d22b24e94952c3520543193296d60c3880e9
MidhunaMohanraj <97339582+MidhunaMohanraj@users.noreply.github.com>
2026-07-20 16:56:50 -0700

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -9,8 +9,6 @@ import click
 from logsentry.clustering import cluster_records
 from logsentry.generator import generate_report
 from logsentry.parser import parse_lines
-
-
 @click.command()
 @click.argument("logfile", type=click.Path(exists=True, dir_okay=False, path_type=Path))
 @click.option("-o", "--output", type=click.Path(path_type=Path), default="log_report.md", help="Output Markdown file.")
